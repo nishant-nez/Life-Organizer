7c7d4217cdeb96202ec9f5492f09f82e32d98608
nishant-nez <nishant.khadka@deerwalk.edu.np>
2023-07-29 19:14:50 +0545

    added dashboard and reminder apps

---
+++ dashboard/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# database returns example list
+contents = [
+    {
+        'author': 'nknk',
+        'title':'my first post',
+        'content': "This is my very first blogpost!",
+        'date_posted': 'July 28, 2023'
+    },
+    {
+        'author': 'khkh',
+        'title':'my second post',
+        'content': "This is my second blogpost!",
+        'date_posted': 'July 27, 2023'
+    }
+]
+
+def home(request):
+    # return HttpResponse('<h1>Welcome to the Dashboard!</h1>')
+    context = {
+        'contents': contents
+    }
+    # pass the data and access within the template
+    return render(request, 'dashboard/index.html', context)
